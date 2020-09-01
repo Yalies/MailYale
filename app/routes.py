4fc97def007d21ab14d74e60454dc37dfb1a5aad
ErikBoesen <me@erikboesen.com>
2020-09-01 16:36:39 -0600

    Fix task running

--- app/routes.py
+++ app/routes.py
@@ -46,5 +46,6 @@ def scraper():
     if request.method == 'GET':
         return render_template('scraper.html')
     payload = request.get_json()
-    tasks.scrape.delay(payload['cookie'])
+    tasks.scrape.apply_async(args=[payload['cookie']])
+    return '', 200
 
--- app/static/js/scraper.js
+++ app/static/js/scraper.js
@@ -1,20 +1,33 @@
 let cookie = document.getElementById('cookie'),
     submit = document.getElementById('submit');
 
+cookie.onchange = function() {
+    submit.disabled = Boolean(cookie.textContent);
+}
+
 submit.onclick = function() {
-    let payload = {
-        'cookie': cookie.value,
-    };
-    fetch('/scraper', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload),
-    })
-    /*
-        .then(response => {
-            response.ok
-        )
-    */
+    if (submit.classList.length == 0) {
+        console.log('Trying to start scraper.');
+        let payload = {
+            'cookie': cookie.value,
+        };
+        fetch('/scraper', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload),
+        }).then(response => {
+            if (response.ok) {
+                submit.textContent = 'Scraper started!';
+                submit.classList.add('success');
+            } else {
+                submit.textContent = 'Scraper run failed.';
+                submit.classList.add('fail');
+            }
+        });
+    } else {
+        submit.textContent = 'Run Scraper';
+        submit.className = '';
+    }
 }
