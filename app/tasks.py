35858203baf6e80807e739ace834a49b428d22ce
ErikBoesen <me@erikboesen.com>
2020-09-01 13:30:41 -0600

    Put scraping into a task

--- app/tasks.py
+++ app/tasks.py
@@ -1,90 +1,95 @@
 from app import app, db, celery
+from app.models import Student
 
+import os
 import time
+import re
+from bs4 import BeautifulSoup
 
 DEBUG = False
 
-cookie = os.environ['YALE_COOKIE']
+def scrape():
+    cookie = os.environ['YALE_COOKIE']
 
-headers = {
-    'Cookie': cookie,
-}
-
-filename = 'page.html'
-if not os.path.exists(filename):
-    print('Page not cached, fetching...')
-    params = {
-        'currentIndex': -1,
-        'numberToGet': 12,
+    headers = {
+        'Cookie': cookie,
     }
-    if not DEBUG:
-        params.update({
+
+    filename = 'page.html'
+    if not os.path.exists(filename):
+        print('Page not cached, fetching...')
+        params = {
             'currentIndex': -1,
-            'numberToGet': -1,
-        })
+            'numberToGet': 12,
+        }
+        if not DEBUG:
+            params.update({
+                'currentIndex': -1,
+                'numberToGet': -1,
+            })
 
-    r = requests.get('https://students.yale.edu/facebook/PhotoPageNew',
-                     params=params,
-                     headers=headers)
-    page_text = r.text
-    with open(filename, 'w') as f:
-        f.write(page_text)
-    print('done.')
-else:
-    with open(filename, 'r') as f:
-        print('Loading cached page... ', end='')
-        page_text = f.read()
+        r = requests.get('https://students.yale.edu/facebook/PhotoPageNew',
+                         params=params,
+                         headers=headers)
+        page_text = r.text
+        with open(filename, 'w') as f:
+            f.write(page_text)
         print('done.')
+    else:
+        with open(filename, 'r') as f:
+            print('Loading cached page... ', end='')
+            page_text = f.read()
+            print('done.')
 
-# Parsing page
+    # Parsing page
 
-RE_BIRTHDAY = re.compile(r"^[A-Z][a-z]{2} \d{1,2}$")
+    RE_BIRTHDAY = re.compile(r"^[A-Z][a-z]{2} \d{1,2}$")
 
-print('Building BeautifulSoup tree.')
-page = BeautifulSoup(page_text, "html.parser")
-containers = page.find_all("div", {"class": "student_container"})
-students = []
-for container in containers:
-    info = container.find_all("div", {"class": "student_info"})
-    name = container.find("h5", {"class": "yalehead"}).text.strip()
-    print("Parsing " + name)
-    surname, forename = name.split(", ", 1)
-    surname = surname.strip()
-    forename = forename.strip()
-    college = info[0].text
-    try:
-        email = info[1].find("a").text
-    except AttributeError:
-        email = ""
-    trivia = info[1].find_all(text=True, recursive=False)
-    try:
-        room = trivia.pop(0)
-        birthday = trivia.pop()
-        major = trivia.pop()
-        address = "\n".join(trivia)
-    except IndexError:
-        room = ""
-        birthday = ""
-        major = ""
-        address = ""
+    print('Building BeautifulSoup tree.')
+    page = BeautifulSoup(page_text, "html.parser")
+    containers = page.find_all("div", {"class": "student_container"})
+    students = []
+    for container in containers:
+        info = container.find_all("div", {"class": "student_info"})
+        name = container.find("h5", {"class": "yalehead"}).text.strip()
+        print("Parsing " + name)
+        surname, forename = name.split(", ", 1)
+        surname = surname.strip()
+        forename = forename.strip()
+        college = info[0].text
+        try:
+            email = info[1].find("a").text
+        except AttributeError:
+            email = ""
+        trivia = info[1].find_all(text=True, recursive=False)
+        try:
+            room = trivia.pop(0)
+            birthday = trivia.pop()
+            major = trivia.pop()
+            address = "\n".join(trivia)
+        except IndexError:
+            room = ""
+            birthday = ""
+            major = ""
+            address = ""
 
-    image_id = container.find("div", {"class": "student_img"}).find("img")["src"][len("/facebook/Photo?id="):]
+        image_id = container.find("div", {"class": "student_img"}).find("img")["src"][len("/facebook/Photo?id="):]
 
-    # Clear all students
-    Student.query.delete()
-    student = Student(
-        id=image_id,
-        forename=forename,
-        surname=surname,
-        year=int(container.find("div", {"class": "student_year"}).text.replace("'", "20")),
-        college=college,
-        pronoun=container.find("div", {"class": "student_info_pronoun"}).text,
-        email=email,
-        room=room,
-        birthday=birthday,
-        major=major,
-        address=address,
-    )
-    db.session.add(student)
+        # Clear all students
+        Student.query.delete()
+        student = Student(
+            id=image_id,
+            forename=forename,
+            surname=surname,
+            year=int(container.find("div", {"class": "student_year"}).text.replace("'", "20")),
+            college=college,
+            pronoun=container.find("div", {"class": "student_info_pronoun"}).text,
+            email=email,
+            room=room,
+            birthday=birthday,
+            major=major,
+            address=address,
+        )
+        db.session.add(student)
 
-db.session.commit()
+    db.session.commit()
