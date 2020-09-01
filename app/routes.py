c8e8337725bae145bf0b9c0d9416b18a6d2867cc
ErikBoesen <me@erikboesen.com>
2020-09-01 14:00:18 -0600

    Add colleges for template

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,23 @@ import json
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    colleges = [
+        'Berkeley College',
+        'Branford College',
+        'Davenport College',
+        'Ezra Stiles College',
+        'Jonathan Edwards College',
+        'Benjamin Franklin College',
+        'Grace Hopper College',
+        'Morse College',
+        'Pauli Murray College',
+        'Pierson College',
+        'Saybrook College',
+        'Silliman College',
+        'Timothy Dwight College',
+        'Trumbull College',
+    ]
+    return render_template('index.html', colleges=colleges)
 
 @app.route('/scraper')
 def scraper():
