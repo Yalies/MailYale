fc0340b820128607b34bfb0bd06764e4f6869beb
ErikBoesen <me@erikboesen.com>
2020-09-01 13:34:26 -0600

    Add configuration for SQLAlchemy

--- config.py
+++ config.py
@@ -4,5 +4,11 @@ import os
 REDIS = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
+    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT', 'Override this too')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                             'sqlite:///' + os.path.join(basedir, 'app.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     CELERY_BROKER_URL = REDIS
     CELERY_RESULT_BACKEND = REDIS
