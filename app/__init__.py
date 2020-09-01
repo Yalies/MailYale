50972c44434ab7e34d9ee16ea002feb279e5601e
ErikBoesen <me@erikboesen.com>
2020-09-01 13:28:25 -0600

    Add db/migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from celery import Celery
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -9,6 +11,8 @@ app.config.from_object(Config)
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 cors = CORS(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes, errors, api
 app.register_blueprint(api.api_bp, url_prefix='/api')
