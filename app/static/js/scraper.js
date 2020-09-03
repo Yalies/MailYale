f74973a497a3756b287f23f70f09092c924c8132
ErikBoesen <me@erikboesen.com>
2020-09-02 22:52:30 -0600

    Automatically unclick button on scraper page

--- app/static/js/scraper.js
+++ app/static/js/scraper.js
@@ -6,28 +6,27 @@ cookie.onchange = function() {
 }
 
 submit.onclick = function() {
-    if (submit.classList.length == 0) {
-        console.log('Trying to start scraper.');
-        let payload = {
-            'cookie': cookie.value,
-        };
-        fetch('/scraper', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload),
-        }).then(response => {
-            if (response.ok) {
-                submit.textContent = 'Scraper started!';
-                submit.classList.add('success');
-            } else {
-                submit.textContent = 'Scraper run failed.';
-                submit.classList.add('fail');
-            }
-        });
-    } else {
-        submit.textContent = 'Run Scraper';
-        submit.className = '';
-    }
+    console.log('Trying to start scraper.');
+    let payload = {
+        'cookie': cookie.value,
+    };
+    fetch('/scraper', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+    }).then(response => {
+        if (response.ok) {
+            submit.textContent = 'Scraper started!';
+            submit.classList.add('success');
+        } else {
+            submit.textContent = 'Scraper run failed.';
+            submit.classList.add('fail');
+        }
+        setTimeout(function() {
+            submit.textContent = 'Run Scraper';
+            submit.className = '';
+        }, 1500);
+    });
 }
