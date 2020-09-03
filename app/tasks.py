b37e8b91130b317237b320ffea53d976422ae40f
ErikBoesen <me@erikboesen.com>
2020-09-02 21:29:19 -0600

    Refactor HTML fetching process out of scraping

--- app/tasks.py
+++ app/tasks.py
@@ -8,6 +8,30 @@ from bs4 import BeautifulSoup
 import usaddress
 
 
+def get_html():
+    filename = 'page.html'
+    if not os.path.exists(filename):
+        print('Page not cached, fetching...')
+
+        r = requests.get('https://students.yale.edu/facebook/PhotoPageNew',
+                         params={
+                             'currentIndex': -1,
+                             'numberToGet': -1,
+                         },
+                         headers={
+                             'Cookie': cookie,
+                         })
+        page_text = r.text
+        with open(filename, 'w') as f:
+            f.write(page_text)
+        print('done.')
+    else:
+        with open(filename, 'r') as f:
+            print('Loading cached page... ', end='')
+            page_text = f.read()
+            print('done.')
+
+
 def parse_address(address):
     # Remove duplicates
     address = list(dict.from_keys(address))
@@ -21,34 +45,7 @@ def parse_address(address):
 
 @celery.task
 def scrape(cookie):
-    headers = {
-        'Cookie': cookie,
-    }
-
-    filename = 'page.html'
-    if not os.path.exists(filename):
-        print('Page not cached, fetching...')
-        params = {
-            'currentIndex': -1,
-            'numberToGet': -1,
-        }
-
-        r = requests.get('https://students.yale.edu/facebook/PhotoPageNew',
-                         params=params,
-                         headers=headers)
-        page_text = r.text
-        with open(filename, 'w') as f:
-            f.write(page_text)
-        print('done.')
-    else:
-        with open(filename, 'r') as f:
-            print('Loading cached page... ', end='')
-            page_text = f.read()
-            print('done.')
-
-    # Parsing page
-
-    RE_BIRTHDAY = re.compile(r'^[A-Z][a-z]{2} \d{1,2}$')
+    html = get_html()
 
     print('Building BeautifulSoup tree.')
     page = BeautifulSoup(page_text, 'html.parser')
@@ -69,6 +66,7 @@ def scrape(cookie):
             email = ''
         trivia = info[1].find_all(text=True, recursive=False)
         RE_ROOM = re.compile(r'^([A-Z]+)-([A-Z]+)(\d+)(\d)([A-Z]+)?$')
+        RE_BIRTHDAY = re.compile(r'^[A-Z][a-z]{2} \d{1,2}$')
         with open('app/res/majors.txt') as f:
             majors = f.read().splitlines()
         try:
@@ -76,6 +74,7 @@ def scrape(cookie):
                 room = trivia.pop(0)
             else:
                 room = None
+
             birthday = trivia.pop()
             major = trivia.pop() if trivia[-1] in majors else None
             address = ', '.join(trivia)
