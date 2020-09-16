affadf3cd7961aad1f8e6f446e342183609c1db9
ErikBoesen <me@erikboesen.com>
2020-09-15 23:58:34 -0600

    Disable leave data parsing

--- app/tasks.py
+++ app/tasks.py
@@ -123,6 +123,7 @@ def scrape(cookie):
 
         db.session.add(student)
 
+    """
     with open('pre2020.html', 'r') as f:
         html = f.read()
     tree = get_tree(html)
@@ -138,6 +139,7 @@ def scrape(cookie):
         student = Student.query.filter_by(email=email).first()
         if student is not None and student.year is not None:
             student.leave = (year < student.year)
+    """
 
     db.session.commit()
     print('Done.')
