ac60b768559dc12296d56dabd6706ed4ff8fbd5a
ErikBoesen <me@erikboesen.com>
2021-02-21 18:28:02 -0500

    Remove Student model

--- app/models.py
+++ app/models.py
@@ -7,25 +7,3 @@ class User(db.Model):
     username = db.Column(db.String, primary_key=True)
     registered_on = db.Column(db.Integer)
     last_seen = db.Column(db.Integer)
-
-
-class Student(db.Model):
-    __tablename__ = 'students'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    forename = db.Column(db.String, nullable=False)
-    surname = db.Column(db.String, nullable=False)
-    year = db.Column(db.Integer)
-    college = db.Column(db.String)
-    pronoun = db.Column(db.String)
-    email = db.Column(db.String, nullable=False)
-    building_code = db.Column(db.String)
-    entryway = db.Column(db.String)
-    floor = db.Column(db.Integer)
-    suite = db.Column(db.Integer)
-    room = db.Column(db.String)
-    birthday = db.Column(db.String)
-    major = db.Column(db.String)
-    address = db.Column(db.String)
-    state = db.Column(db.String)
-    leave = db.Column(db.Boolean, default=False)
