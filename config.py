af4a4ad678bc9587657fb8553bb32983cd2f6426
ErikBoesen <me@erikboesen.com>
2021-02-21 23:27:58 -0500

    Add SECRET_KEY back to config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                                              'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
