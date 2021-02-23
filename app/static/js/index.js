370453f9c6885f3624aff4605075707a78cd0b87
ErikBoesen <me@erikboesen.com>
2021-02-22 20:02:16 -0500

    Borrow Yalies filter element management JS code

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,38 +1,59 @@
 const MAX_EMAILS_PER_DAY = 2000;
 
-let body = document.body,
-    checkboxes = document.querySelectorAll('input[type="checkbox"]'),
-    allCheckboxes = document.querySelectorAll('input[type="checkbox"][name$="-all"]'),
-    output = document.getElementById('output');
+let p = {
+    body: document.body,
+    checkboxes: document.querySelectorAll('input[type="checkbox"]'),
+    allCheckboxes: document.querySelectorAll('input[type="checkbox"][name$="-all"]'),
+    output: document.getElementById('output'),
+    query: document.getElementById('query'),
+    submit: document.getElementById('submit'),
+    filters: document.getElementsByClassName('filter'),
+    clearFilters: document.getElementById('clear_filters'),
+    list: document.getElementById('list'),
+    loading: document.getElementById('loading'),
+    empty: document.getElementById('empty'),
+};
 
-for (let checkbox of checkboxes) {
-    checkbox.checked = false;
-}
-for (let checkbox of allCheckboxes) {
-    checkbox.checked = true;
+function collapseAllFilters() {
+    for (let filter of p.filters) {
+        filter.classList.add('collapsed');
+    }
 }
 
-onchange = function(e) {
-    let input = e.target;
-    if (input.type === 'checkbox') {
-        let checked = input.checked;
-        let otherCheckboxes = Array.from(input.parentElement.parentElement.getElementsByTagName('input'));
-        let allCheckbox = otherCheckboxes.shift();
-        if (input == allCheckbox) {
-            for (let checkbox of otherCheckboxes) {
-                checkbox.checked = !checked;
-            }
-        } else {
-            if (checked) {
-                allCheckbox.checked = false;
-            } else {
-                let anyChecked = false;
-                for (let checkbox of otherCheckboxes) {
-                    anyChecked = anyChecked || checkbox.checked;
-                }
-                allCheckbox.checked = !anyChecked;
-            }
-        }
+function resetFilters() {
+    for (let filter of p.filters) {
+        filter.classList.add('collapsed');
+        filter.classList.remove('active');
+    }
+    for (let checkbox of p.checkboxes) {
+        checkbox.checked = false;
+    }
+    for (let checkbox of p.allCheckboxes) {
+        checkbox.checked = true;
+    }
+}
+resetFilters();
+
+p.clearFilters.onclick = function() {
+    resetFilters();
+}
+
+function isFilter(element) {
+    return element.tagName === 'DIV' && element.classList.contains('filter');
+}
+
+onclick = function(e) {
+    let filter = null;
+    if (isFilter(e.target)) {
+        filter = e.target;
+    } else if (e.target.tagName === 'H4' && isFilter(e.target.parentElement)) {
+        filter = e.target.parentElement;
+    } else if (e.target.tagName === 'I' && isFilter(e.target.parentElement.parentElement)) {
+        filter = e.target.parentElement.parentElement;
+    }
+
+    if (filter) {
+        filter.classList.toggle('collapsed');
     }
 };
 
@@ -42,9 +63,9 @@ let submit = document.getElementById('submit'),
 
 submit.onclick = function() {
     let filters = {};
-    for (let section of sections) {
-        let category = section.id;
-        let otherCheckboxes = Array.from(section.getElementsByTagName('input'));
+    for (let filter of p.filters) {
+        let category = filter.id;
+        let otherCheckboxes = Array.from(filter.getElementsByTagName('input'));
         let allCheckbox = otherCheckboxes.shift();
         if (!allCheckbox.checked) {
             filters[category] = []
