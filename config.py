6c14f595994e04479f8bf08be5aa697ec57772b4
ErikBoesen <me@erikboesen.com>
2022-10-13 12:14:48 -0400

    Fix postgres URI

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
-                                             'sqlite:///' + os.path.join(basedir, 'app.db'))
+                                             'sqlite:///' + os.path.join(basedir, 'app.db')).replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CAS_SERVER = 'https://secure.its.yale.edu'
